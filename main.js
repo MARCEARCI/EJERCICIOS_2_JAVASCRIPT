21e15ee13be49f07b417982c176ed162bd353d1d
MARCEARCI <143982722+MARCEARCI@users.noreply.github.com>
2023-11-07 14:10:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+/*
+console.log("---------------------ejercicio 1----------------------");
+//ejercicio 1//
+let num1 = prompt("ingrese el primer numero numero")
+let num2 = prompt("ingrese el segundo numero")
+if (num1>num2) {
+    console.log("el primer numero ingresado es mayor");  
+} 
+
+console.log("---------------------ejercicio 2----------------------");
+//ejercicio 2//
+let num1_e2 = prompt("ingrese el primer numero a comparar")
+let num2_e2 = prompt("ingrese el segundo numero a comparar")
+if (num1_e2==num2_e2) {
+    console.log("los numeros ingresados son iguales");
+} else {
+    console.log("los numeros ingresados son diferentes");
+}
+
+console.log("---------------------ejercicio 3----------------------");
+//ejercicio 3//
+let num1_e3 = prompt("ingrese el primer numero a comparar")
+let num2_e3 = prompt("ingrese el segundo numero a comparar")
+if (num1_e3 == num2_e3) {
+    console.log("los numeros ingresados son iguales");
+}
+else {
+    if (num1_e3 > num2_e3) {
+        console.log("el primer numero ingresado es mayor");
+    }
+            else {
+        console.log("el segundo numero es mayor")
+    }
+}
+   
+console.log("---------------------ejercicio 4----------------------");
+//ejercicio 4//
+let num1_e4 = prompt("ingrese el primer numero a comparar")
+let num2_e4 = prompt("ingrese el segundo numero a comparar")
+let num3_e4 = prompt("ingrese el tercer numero a comparar")
+if (num1_e4 < num2_e4 && num1_e4<num3_e4) {
+    console.log("el primer numero ingresado es el menor")
+
+} else {
+    if (num2_e4 < num3_e4 && num2_e4<num1_e4) {
+        console.log("el segundo numero ingresado es el menor")
+    } else {
+        if (num3_e4 < num2_e4 && num3_e4<num1_e4) {
+            console.log("el tercer numero ingresado es el menor")
+        } else {
+            console.log("los tres numeros son iguales")
+        }
+    }
+}
+
+console.log("---------------------ejercicio 5----------------------");
+//ejercicio 5//
+const persona_e51 = [
+    {
+        nombre: prompt("ingrese el nombre de la primera persona"),
+        edad: prompt("ingrese la edad de la primera persona"),
+        altura: prompt("ingrese la altura de la primera persona")
+    }
+]
+const persona_e52 = [
+    {
+        nombre: prompt("ingrese el nombre de la segunda persona"),
+        edad: prompt("ingrese la edad de la segunda persona"),
+        altura: prompt("ingrese la altura de la segunda persona")
+    }
+]
+console.log(persona_e51);
+console.log(persona_e52);
+
+if (persona_e51[1] > persona_e52[1]) {
+    console.log("la primera persona es mayor")
+} else {
+    console.log("la segunda persona es mayor")
+}
+
+if (persona_e51[2] > persona_e52[2]) {
+    console.log("la primera persona es mas alta");
+} else {
+    console.log("la segunda persona es mas alta");
+} 
+*/
+
+console.log("---------------------ejercicio 6----------------------");
+//ejercicio 6//
+
+
